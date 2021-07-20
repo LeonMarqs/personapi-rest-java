a12f3a9c053093f4497dc59af00095d1e0cfb6e6
LeonMarqs <leomarques301@gmail.com>
2021-07-20 20:42:56 -0300

    Add find one by id (GET method)

--- src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
+++ src/main/java/one/digitalinnovation/personapi/controller/PersonController.java
@@ -2,7 +2,7 @@ package one.digitalinnovation.personapi.controller;
 
 import one.digitalinnovation.personapi.dto.MessageResponseDTO;
 import one.digitalinnovation.personapi.dto.request.PersonDTO;
-import one.digitalinnovation.personapi.entity.Person;
+import one.digitalinnovation.personapi.exception.PersonNotFoundException;
 import one.digitalinnovation.personapi.service.PersonService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -29,5 +29,11 @@ public class PersonController {
 	@GetMapping
 	public List<PersonDTO> listAll() {
 		return personSevice.listAll();
+	}
+	
+	@GetMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
+		return personSevice.findById(id);
 	}
 }
